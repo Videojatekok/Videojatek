e6af665db0649a47922b076bca14a24d0f98102f
FazekasBalint <fazekas.balint@students.jedlik.eu>
2024-03-20 10:40:34 +0100

    vege

--- Projekt/kep_csere.js
+++ Projekt/kep_csere.js
@@ -1,8 +1,10 @@
 const pong = document.querySelector('.pong')
+const tennis = document.querySelector('.tennis')
+const space = document.querySelector('.space')
 
 let faceNumber=1;
 
-function nextFace(){
+function PongnextFace(){
     pong.src=`kepek/alaphtmlkepek/pong_${faceNumber}.png`;
     faceNumber++;
     if(faceNumber==4){
@@ -10,4 +12,27 @@ function nextFace(){
     }
 }
 
-pong.addEventListener('click',nextFace)
+
+
+
+function SpacenextFace(){
+    space.src=`kepek/alaphtmlkepek/space_${faceNumber}.png`;
+    faceNumber++;
+    if(faceNumber==4){
+        faceNumber=1;
+    }
+}
+
+
+
+function tennisnextFace(){
+    tennis.src=`kepek/alaphtmlkepek/tennis_${faceNumber}.png`;
+    faceNumber++;
+    if(faceNumber==4){
+        faceNumber=1;
+    }
+}
+
+tennis.addEventListener('click',tennisnextFace)
+space.addEventListener('click',SpacenextFace)
+pong.addEventListener('click',PongnextFace)
